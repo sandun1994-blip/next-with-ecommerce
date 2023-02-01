cad824e16f1546d1062493667f81da9f4c863bea
Sandun Tharuka <sandun.suntws@gmail.com>
2023-02-02 01:22:53 +0530

    SETUP LOGOUT

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,12 @@
 import { Store } from '@/utils/Store'
-import { useSession } from 'next-auth/react'
+import { Menu } from '@headlessui/react'
+import Cookies from 'js-cookie'
+import { signOut, useSession } from 'next-auth/react'
 import Head from 'next/head'
 import Link from 'next/link'
 import React, { useContext, useEffect, useState } from 'react'
 import { ToastContainer } from 'react-toastify'
+
 
 export default function Layout({ title, children }) {
     const {status,data:session} =useSession()
@@ -19,7 +22,11 @@ setCartItemsCount(cart.cartItems.reduce((a,c)=>a+c.quantity,0))
 },[cart.cartItems])
 
 
-
+const logoutClickHandler =()=>{
+    Cookies.remove('cart')
+    dispatch({type:'CART_RESET'})
+    signOut({callbackUrl:'/login'})
+}
 
 
 
@@ -45,7 +52,49 @@ setCartItemsCount(cart.cartItems.reduce((a,c)=>a+c.quantity,0))
                                 {cartItemsCount}
                             </span>)}</Link>
                              {status==='loading'?('loading'):
-                            (session?.user?session.user.name:(<Link href={'/login'} className='p-2'>Login</Link>))}
+                            (session?.user?
+                                <Menu as="div" className="relative inline-block">
+                                <Menu.Button className="text-blue-600">
+                                  {session.user.name}
+                                </Menu.Button>
+                                <Menu.Items className="absolute right-0 w-56 origin-top-right bg-white  shadow-lg ">
+                                  <Menu.Item>
+                                    <Link className="dropdown-link" href="/profile">
+                                      Profile
+                                    </Link>
+                                  </Menu.Item>
+                                  <Menu.Item>
+                                    <Link
+                                      className="dropdown-link"
+                                      href="/order-history"
+                                    >
+                                      Order History
+                                    </Link>
+                                  </Menu.Item>
+                                  {session.user.isAdmin && (
+                                    <Menu.Item>
+                                      <Link
+                                        className="dropdown-link"
+                                        href="/admin/dashboard"
+                                      >
+                                        Admin Dashboard
+                                      </Link>
+                                    </Menu.Item>
+                                  )}
+                                  <Menu.Item>
+                                    <a
+                                      className="dropdown-link"
+                                      href="#"
+                                      onClick={logoutClickHandler}
+                                    >
+                                      Logout
+                                    </a>
+                                  </Menu.Item>
+                                </Menu.Items>
+                              </Menu>
+                            
+                            
+                            :(<Link href={'/login'} className='p-2'>Login</Link>))}
                         </div>
                     </nav>
                 </header>
